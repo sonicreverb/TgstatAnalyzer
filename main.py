8ab01b347218a0ce58d5e92979722037646283c0
Vladislav <vladislav.krasivin@gmail.com>
2023-11-19 16:47:24 +0300

    автоматическая выборка по дате, предотвращение дубликатов, получение ссылок на ресурсы, используемые в публикации и форматирование ссылок в таблице.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import configparser
-import pickle
+import time
+
 import tables_managment as tables
+
+from datetime import datetime
 from api_utils import get_API_response, token_key
 
 key_words = []
@@ -35,6 +38,8 @@ try:
 except Exception as ex:
     print(f"[ЧТЕНИЕ ПАРАМЕТРОВ ИЗ ФАЙЛА КОНФИГУРАЦИИ] Возникла ошибка! ({ex}).")
 
+OUTPUT_FILENAME = 'TgstatAnalyzer_output.xlsx'
+
 
 def main():
     # проверка на валидность чтения из конфига
@@ -43,10 +48,36 @@ def main():
         return None
 
     # список полученных ответов от API.
-    responses_list = []
+    responses_list = {}
+
+    # определяем дату последней найденной публикации для каждого ключевого слова в таблице
+    keywordsXLSX_data = tables.xlsx_connector.get_column_data(9)
+    publish_data_XLSX = tables.xlsx_connector.get_column_data(8)
+    last_publish_data_dict = {}
+
+    for row_num in range(1, len(keywordsXLSX_data)):
+        date_xlsx = publish_data_XLSX[row_num]
+        current_keyword = keywordsXLSX_data[row_num]
+
+        if not isinstance(date_xlsx, str):
+            timestamp = date_xlsx.timestamp()
+        else:
+            datetime_object = datetime.strptime(date_xlsx, "%d/%m/%Y %H:%M:%S")
+            timestamp = datetime.timestamp(datetime_object)
+
+        if current_keyword not in last_publish_data_dict:
+            last_publish_data_dict[current_keyword] = timestamp
+        else:
+            if last_publish_data_dict[current_keyword] < timestamp:
+                last_publish_data_dict[current_keyword] = timestamp
 
     # проход по каждому ключевому слову
     for search_text in key_words:
+        if config_options.get('endDate', '0') == '0':
+            if search_text in last_publish_data_dict:
+                config_options['endDate'] = int(time.time())
+                config_options['startDate'] = int(last_publish_data_dict.get(search_text, 0))
+        responses_list[search_text] = []
         try:
             current_offset = 0
 
@@ -54,6 +85,13 @@ def main():
             search_params = {'token': token_key, 'q': search_text, 'offset': '0'}
             search_params.update(config_options)
             search_params['limit'] = 50
+
+            if search_params['startDate'] != '0' and search_params['endDate'] != '0':
+                print(f"[MAIN] Поиск публикаций во временном промежутке с"
+                      f" [{datetime.fromtimestamp(float(search_params['startDate']))}] "
+                      f"по [{datetime.fromtimestamp(float(search_params['endDate']))}].")
+            else:
+                print(f'[MAIN] Поиск публикаций во временном промежутке по умолчанию.')
 
             # ракировка limit условного (result_posts_num = тотальное кол-во постов)
             # и limit API (для API максимум 50 постов за запрос)
@@ -64,56 +102,52 @@ def main():
 
             while (result_posts_num / (current_offset + 1)) >= 1:
                 res = get_API_response('https://api.tgstat.ru/posts/search', search_params).json()
-                responses_list.append(res)
-                print(f'[MAIN] Запрос по ключевому слову ({search_text}) вернул состояние {res.get("status")}.', end='')
-                print(res)
+                responses_list[search_text].append(res)
+                print(f'[MAIN] Запрос по ключевым словам "{search_text}" вернул состояние {res.get("status")}.', end='')
                 if res.get("response", None):
                     if res['response'].get('count') != 50:
-                        print(f"[{current_offset + res['response'].get('count') }/{res['response']['total_count']}]")
+                        print(f"[{current_offset + res['response'].get('count') }/{res['response']['total_count']}] "
+                              f"(limit = {result_posts_num}).",
+                              end='')
+                        print('\n', end='')
                         break
-                    print(f"[{current_offset}/{res['response'].get('total_count')}]")
-
+                    print(f"[{current_offset}/{res['response'].get('total_count')}] "
+                          f"(limit = {result_posts_num}).", end='')
+                print('\n', end='')
                 current_offset += 50
                 search_params['offset'] = current_offset
         except Exception as _ex:
             print(f"[MAIN] Во время попытки получить данные по ключевому слову {search_text} возникла ошибка! ({_ex}).")
 
-    with open('tables_managment/responses.pkl', 'wb') as file:
-        pickle.dump(responses_list, file)
-
     output_data = []
     # запись в таблицу
-    for resp in responses_list:
-        try:
-            print(resp)
-            data = resp.get('response', {}).get('items', None)
-            if not data:
-                print(f"API response bad status ({resp.get('status')}).")
+    for search_key in responses_list:
+        for resp in responses_list[search_key]:
+            try:
+                data = resp.get('response', {}).get('items', None)
+                if not data:
+                    print(f"[АНАЛИЗ ПУБЛИКАЦИИ] Информация не найдена. API response status ({resp}")
 
-            else:
-                for elem in data:
-                    try:
-                        output_data.append(tables.get_item_data(elem))
-                    except Exception as _ex:
-                        print([f'[АНАЛИЗ ПУБЛИКАЦИИ] Во время попытки получить информацию из публикации было '
-                               f'вызвано исключение ({_ex})'])
+                else:
+                    for elem in data:
+                        try:
+                            info_dict = tables.get_item_data(elem)
+                            info_dict['keyWord'] = search_key
+                            output_data.append(info_dict)
+                        except Exception as _ex:
+                            print([f'[АНАЛИЗ ПУБЛИКАЦИИ] Во время попытки получить информацию из публикации было '
+                                   f'вызвано исключение ({_ex})'])
 
-        except Exception as _ex:
-            print(f"Во время обработки полученного ответа от API было вызвано исключение ({_ex}).")
+            except Exception as _ex:
+                print(f"Во время обработки полученного ответа от API было вызвано исключение ({_ex}).")
     if output_data:
-        tables.write_to_excel('TgstatAnalyzer_output.xlsx', output_data)
+        tables.write_to_excel(OUTPUT_FILENAME, output_data)
 
 
 if __name__ == "__main__":
-    # main()
-    with open('tables_managment/responses.pkl', 'rb') as file:
-        li = []
-        for loaded_variable in pickle.load(file):
-            print(loaded_variable)
-            for item2 in loaded_variable.get('response', {}).get('items', [None]):
-                if item2:
-                    li.append(tables.get_item_data(item2))
-                else:
-                    print("ERROR!")
-
-        tables.write_to_excel('test_output.xlsx', li)
+    try:
+        main()
+    except Exception as ex:
+        print(f'Критическая ошибка! {ex}')
+    print('Для завершения программы нажмите ENTER...')
+    input()
--- tables_managment/xlsx_connector.py
+++ tables_managment/xlsx_connector.py
@@ -1,10 +1,20 @@
-from datetime import datetime
 import openpyxl
 import os
+import re
+
+from datetime import datetime
+from openpyxl.styles import Font
+
+OUTPUT_FILENAME = 'TgstatAnalyzer_output.xlsx'
 
 
 # принимает на вход API class Post, возвращает словарь с необходимыми хар-ками публикации
 def get_item_data(item):
+    # название канала
+    post_id_name = item['link'].split('/')[1]
+    if post_id_name == "c":
+        post_id_name = item['link'].split('/')[2]
+
     # ссылка на сообщение
     post_url = "https://" + item['link']
 
@@ -30,13 +40,41 @@ def get_item_data(item):
     # ссылка на источник
     post_source_link = post_url[:post_url.rfind('/')]
 
+    # ссылки на сторонние ресурсы
+    url_pattern = re.compile(r'<a\b[^>]*href=[\'"]?([^\'" >]+)[^>]*>(.*?)</a>|https?://\S+')
+    matches = re.finditer(url_pattern, post_text)
+    links = [match.group(1) if match.group(1) else match.group(0) for match in matches]
+
+    post_rel_resourses = ''
+    for link in links:
+        if link[:-1] == "'":
+            link = link[:-1]
+        post_rel_resourses += link + '\n'
+
+    if post_rel_resourses == '':
+        post_rel_resourses = 'None'
+
     # дата публикации
     timestamp = item['date']
     dt_object = datetime.utcfromtimestamp(timestamp)
     post_date = dt_object.strftime('%d/%m/%Y %H:%M:%S')
 
-    return {'postURL': post_url, 'postText': post_text, 'postImgURL': post_image_url, 'postVideoURL': post_video_url,
-            'postSourceURL': post_source_link, 'postPublishDate': post_date}
+    return {'postSourseName': post_id_name, 'postURL': post_url, 'postText': post_text, 'postImgURL': post_image_url,
+            'postVideoURL': post_video_url, 'postSourceURL': post_source_link, 'relevResoursesURL': post_rel_resourses,
+            'postPublishDate': post_date}
+
+
+def get_column_data(col_num):
+    if os.path.exists(OUTPUT_FILENAME):
+        # если файл существует, открываем его и выбираем активный лист
+        wb = openpyxl.load_workbook(OUTPUT_FILENAME)
+        ws = wb.active
+        # считывание уже присутствующих ссылок в таблице
+        result = [ws.cell(row=row, column=col_num).value for row in range(1, ws.max_row + 1)]
+    else:
+        result = []
+
+    return result
 
 
 def write_to_excel(filename, data):
@@ -45,36 +83,41 @@ def write_to_excel(filename, data):
         # если файл существует, открываем его и выбираем активный лист
         wb = openpyxl.load_workbook(filename)
         ws = wb.active
+        # считывание уже присутствующих ссылок в таблице
+        column_number = 3
+        unique_text_in_table = [ws.cell(row=row, column=column_number).value for row in range(1, ws.max_row + 1)]
     else:
         # если файл не существует, создаем новую книгу Excel и выбираем активный лист
         wb = openpyxl.Workbook()
         ws = wb.active
+        unique_text_in_table = []
+
+    data_to_write = []
+    for item in data:
+        if item.get('postText') not in unique_text_in_table:
+            data_to_write.append(item)
+            unique_text_in_table.append(item.get('postText'))
+
+    if not data_to_write:
+        print(f'[ЗАПИСЬ В ЭКСЕЛЬ] Не найдено новых уникальных публикаций для запись в {filename}')
+        return
 
     # добавляем заголовки таблицы
-    headers = list(data[0].keys())
+    headers = list(data_to_write[0].keys())
     for col_num, header in enumerate(headers, 1):
         ws.cell(row=1, column=col_num, value=header)
 
+    hyperlink_style = Font(color='0000FF', underline='single')
+
     # записываем данные в конец таблицы
-    for row_num, row_data in enumerate(data, ws.max_row + 1):
+    for row_num, row_data in enumerate(data_to_write, ws.max_row + 1):
         for col_num, key in enumerate(headers, 1):
-            ws.cell(row=row_num, column=col_num, value=row_data[key])
+            cell = ws.cell(row=row_num, column=col_num, value=row_data[key])
+            if col_num in [2, 4, 5, 6, 7] and row_data[key] != 'None' and 'TgstatAPI return null' not in row_data[key]:
+                cell.font = hyperlink_style
+                cell.hyperlink = row_data[key]
 
     # сохраняем книгу Excel
     wb.save(filename)
 
-    print(f'[ЗАПИСЬ В ЭКСЕЛЬ] Данные успешно записаны в файл {filename}')
-
-
-# import pickle
-#
-# # Загрузка переменной из файла
-# with open('responses.pkl', 'rb') as file:
-#     loaded_variable_list = pickle.load(file)
-#
-# li = []
-# for loaded_variable in loaded_variable_list:
-#     for item2 in loaded_variable['response']['items']:
-#         li.append(get_item_data(item2))
-#
-# write_to_excel('test_output.xlsx', li)
+    print(f'[ЗАПИСЬ В ЭКСЕЛЬ] Данные успешно записаны в файл {filename} [{len(data_to_write)}/{len(data)}]')
